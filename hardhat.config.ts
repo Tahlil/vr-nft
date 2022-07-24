2def0008a84e522acedc9d15da9cfc7d5b1d54bd
tahlil <bsse0803@iit.du.ac.bd>
2022-07-24 17:40:52 +0600

    Update compile config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-abi-exporter');
+require('dotenv').config()
 
-const config: HardhatUserConfig = {
+// Replace this private key with your Mumbai wallet private key
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+// Replace this with your Datahub api key
+const DATAHUB_API_KEY = process.env.DATAHUB_API_KEY;
+
+
+const config = {
   solidity: "0.8.14",
+  abiExporter: {
+    path: './abi/',
+    clear: true,
+  },
+  networks: {
+    rinkeby: {
+      url: `https://ethereum-rinkeby--rpc.datahub.figment.io//apikey/${DATAHUB_API_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
